665381abee7d43515d4a30dad41f6e1cf6f4cec8
antonsosnin69 <oliverquine@yandex.ru>
2023-01-31 22:41:57 +0300

    add dop zadacha

---
+++ task10/Program.cs
@@ -0,0 +1,42 @@
+﻿// Напишите программу, которая выводит на экран числа от 1 до 100.
+// При этом вместо чисел, кратных трем, программа должна выводить слово "Fizz",
+// а вместо чисел кратных 5 слово "Buzz". 
+// Если число кратно и 3, и 5 то программа должна выводить слово "FizzBuzz".
+
+Console.Clear();
+
+int count = 1;
+while (count <= 100)
+{
+    if ((count % 3 == 0) & (count % 5 == 0))
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.Write("FizzBuzz");
+        Console.ForegroundColor = ConsoleColor.Red;
+    }
+    else if (count % 3 == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.Write("Fizz");
+        Console.ForegroundColor = ConsoleColor.Yellow;
+    }
+    else if (count % 5 == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.Write("Buzz");
+        Console.ForegroundColor = ConsoleColor.Magenta;
+    }
+    else
+    {
+        Console.Write(count);
+    }
+    if (count == 100)
+    {
+        Console.Write(". ");
+    }
+    else
+    {
+        Console.Write(", ");
+    }
+    count++;
+}
