9b1c40cb3531196e59362e7da865ef6fd3fb5c7e
antonsosnin69 <oliverquine@yandex.ru>
2023-02-08 11:41:57 +0300

    add task 15

---
+++ seminar4/task15/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+Console.Clear();
+Console.Write("введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"сумма цифр = {GetSumNums(num)}");
+
+int GetSumNums(int number)
+
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        sum += number;
+        Console.Write(sum + " ");
+        number--;
+    }
+    return sum;
+}
