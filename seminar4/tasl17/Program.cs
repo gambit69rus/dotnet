47e1272d13d746cd025f38a5c1e8438d891d58d5
antonsosnin69 <oliverquine@yandex.ru>
2023-02-08 12:20:57 +0300

    add task 17

--- seminar4/tasl17/Program.cs
+++ seminar4/tasl17/Program.cs
@@ -1,3 +1,18 @@
 ﻿// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
 // 4 -> 24
 // 5 -> 120
+
+Console.Clear();
+Console.Write("введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"произведение чисел = {GetMulNums(num)}");
+
+int GetMulNums(int number)
+{
+    int temp = 1;
+    for (int count = 1; count <= number; count++)
+    {
+        temp = temp * count;
+    }
+    return temp;
+}
