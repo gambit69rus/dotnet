14f8df948185118cc50645c6f83eec84b553b6ed
antonsosnin69 <oliverquine@yandex.ru>
2023-02-12 14:30:04 +0300

    add seminar5

---
+++ seminar5/task19/Program.cs
@@ -0,0 +1,47 @@
+﻿//Задача 32: Напишите программу замена элементов массива: положительны
+//элементы замените на соответствующие отрицательные, и наоборот.
+//[-4, -8, 8, 2] -> [4, 8, -8, -2] 
+
+int size = InputNumber("Введите размер массива");
+int min = InputNumber("Введите минимальное значение в массиве");
+int max = InputNumber("Введите максимальное значение в массиве");
+int[] array = GetArray(size, min, max);
+
+PrintArray(array);
+array = ChangeSign(array);
+PrintArray(array);
+
+int[] GetArray(int length, int min = -1, int max = 1)
+{
+    int[] res = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        res[i] = new Random().Next(min, max);
+    }
+    return res;
+}
+
+int[] ChangeSign(int[] someArray)
+{
+    int[] res = someArray;
+    for (int i = 0; i <= res.Length - 1; i++)
+    {
+        res[i] *= (-1);
+    }
+    return res;
+}
+void PrintArray(int[] someArray)
+{
+    Console.Write("[");
+    for (int i = 0; i <= someArray.Length - 1; i++)
+    {
+        Console.Write($"{someArray[i]}, ");
+    }
+    Console.WriteLine($"{someArray[someArray.Length - 1]}]");
+}
+int InputNumber(string message)
+{
+    Console.Write($"{message}: ");
+    int res = int.Parse(Console.ReadLine());
+    return res;
+}
---
+++ seminar5/task20/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 33: Задайте массив. Напишите программу, которая определяет, 
+// присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// -3; массив [6, 7, 19, 345, 3] -> да
+
+int size = InputNumber("Введите размер массива");
+int min = InputNumber("Введите минимальное значение в массиве");
+int max = InputNumber("Введите максимальное значение в массиве");
+int num = InputNumber("Введите число которое нужно найти в массиве");
+int[] array = GetArray(size, min, max);
+PrintArray(array);
+Console.Write($"число {num} в массиве ");
+if (CheckArrayNum(array, num)) Console.WriteLine("присутствует");
+else Console.WriteLine("отсутствует");
+bool CheckArrayNum(int[] someArray, int checkNum)
+{
+    bool flag = false;
+    for (int i = 0; i <= someArray.Length - 1; i++) if (someArray[i] == checkNum) flag = true;
+    return flag;
+}
+
+int[] GetArray(int length, int min = -1, int max = 1)
+{
+    int[] res = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        res[i] = new Random().Next(min, max);
+    }
+    return res;
+}
+
+void PrintArray(int[] someArray)
+{
+    Console.Write("[");
+    for (int i = 0; i <= someArray.Length - 1; i++)
+    {
+        Console.Write($"{someArray[i]}, ");
+    }
+    Console.WriteLine($"{someArray[someArray.Length - 1]}");
+}
+int InputNumber(string message)
+{
+    Console.Write($"{message}: ");
+    int res = int.Parse(Console.ReadLine());
+    return res;
+}
